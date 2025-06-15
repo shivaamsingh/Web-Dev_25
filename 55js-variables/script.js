dc9d593edfb37927c1f2de9cc1e29ccff725fdf3
shivaamsingh <shivamsinghh088@gamil.com>
2025-06-15 09:14:11 +0530

    new files

--- 55js-variables/script.js
+++ 55js-variables/script.js
@@ -11,3 +11,24 @@ console.log(typeof a,typeof b,typeof c)
 
 // const a1=9;
 // a1=a1+99;  NOT ALLOWED coz a1 is constant
+
+
+let x= "mango bhao";
+let y = 22;
+let z=6.9;
+const c2 = 2;
+let p= true;
+let q= undefined;
+let r=null;
+console.log(x,y,z,p,q,r)
+console.log(typeof(x),typeof(y),typeof(z),typeof(p),typeof(q),typeof(r))
+
+
+let o={
+    name:"mary",
+    "job role":"painter",
+    "is_ugly":false,
+}
+console.log(o);
+o["salary in crores"]="500 crore";
+console.log(o);
---
+++ 56_js_conditionals/script.js
@@ -0,0 +1,20 @@
+// console.log("conditional\n")
+
+let age =5;
+// let grace=2;
+// age+=grace
+
+// console.log(age+"\n")
+// console.log("arithmetic op")
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+if (age>=18) {
+    console.log("u can drive");
+}
+else{
+    console.log("u can't drive");
+}
